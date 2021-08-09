76be4203ac586b86fe8aed752de52ab6d3f3c22c
issk <76851155+ISSKsec@users.noreply.github.com>
2021-08-09 22:16:35 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,9 +3,9 @@
 #sudo apt install knockd
 #GOLPEO DE PUERTOS CON SECUENCIA VARIABLE 
 
-i="62431 8234 3456"
-a="8234 3456 62431"
-b="3456 8234 62431"
+i="62431 8234 3456"  #CHANGE THIS
+a="8234 3456 62431"  #CHANGE THIS
+b="3456 8234 62431"  #CHANGE THIS
 while true 
 do
 knock 10.10.10.83 $i else ;knock 10.10.10.83 $a else; knock 10.10.10.83 $b ; done
